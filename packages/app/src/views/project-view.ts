12a0a84438e75ad913415618a72ae620f3ebeb4e
Reva Moolky <revamoolky@Revas-Laptop.lan>
2025-06-06 02:27:11 -0700

    added amplitude

--- packages/app/src/views/project-view.ts
+++ packages/app/src/views/project-view.ts
@@ -45,8 +45,46 @@ export class ProjectView extends LitElement {
             </section>
           </main>
         `;
+        case "amplitude":
+          return html`
+            <main>
+              <section class="hero">
+                <h1>Building a Research Toolkit for Designer-Led Discovery and PM Collaboration</h1>
+                <ul class="tag-list">
+                  <li class="tag">UX Research</li>
+                  <li class="tag">Interaction Design</li>
+                  <li class="tag">Prototyping</li>
+                </ul>
+                <img src="/images/panw1.png" alt="Palo Alto image" />
+                <p>
+                  The UX design process at NetSec currently lacks sufficient and timely user insights...
+                </p>
+              </section>
+  
+              <empathize-section
+                heading="UNDERSTANDING DESIGNERS & THEIR RESEARCH CHALLENGES"
+                paragraph="I started by conducting a thorough literature review..."
+              ></empathize-section>
+  
+              <empathize-section
+                heading="I RAN 9 QUALITATIVE INTERVIEWS"
+                paragraph="To tailor the toolkit to their specific needs..."
+              ></empathize-section>
+  
+              <section class="cta">
+                <h2>Let's create something amazing together</h2>
+                <p>
+                  I'm always open to discussing new projects, creative ideas...
+                </p>
+                <a href="/app/contact">Get in touch</a>
+              </section>
+            </main>
+          `;
       }
+
+      
     }
+    
 
   static styles = css`
     /* Put your layout styles here */
